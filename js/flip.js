83955cdd24212b1db38f41df85b619f928301978
Sean <shironlai@sina.com>
2017-02-07 16:19:34 +0800

    优化翻转效果

--- js/flip.js
+++ js/flip.js
@@ -3,12 +3,14 @@ var prev = function() {
 	if(1===curPage) return;
 	//左边翻页
 	var lPage = document.querySelector("#gua-flip-pageL" + curPage);
-	lPage.style.transform = "rotateY(180deg)";
-	lPage.style.webkitTransform = "rotateY(180deg)";
+	lPage.style.transform = "rotateY(85deg)";
+	lPage.style.transitionDelay = '0s';
+	// lPage.style.webkitTransform = "rotateY(85deg)";
 	//右边翻页
 	var rPage = document.querySelector("#gua-flip-pageR" + (curPage-1));
 	rPage.style.transform = "rotateY(0deg)";
-	rPage.style.webkitTransform = "rotateY(0deg)";
+	rPage.style.transitionDelay = '0.5s';
+	// rPage.style.webkitTransform = "rotateY(0deg)";
 	curPage--;
 }
 var  next = function() {
@@ -16,9 +18,11 @@ var  next = function() {
 	//左边翻页
 	var lPage = document.querySelector("#gua-flip-pageL" + (curPage+1));
 	lPage.style.transform = "rotateY(0deg)";
+	lPage.style.transitionDelay = '0.5s';
 	//右边翻页
 	var rPage = document.querySelector("#gua-flip-pageR" + curPage);
-	rPage.style.transform = "rotateY(-180deg)";
+	rPage.style.transform = "rotateY(-85deg)";
+	rPage.style.transitionDelay = '0s';
 	curPage++;
 }
 document.querySelector('#prev').addEventListener('click', prev)
